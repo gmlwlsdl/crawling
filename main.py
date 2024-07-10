718c7c108c1e5a862efc2d56e874fac4d2120fa1
gmlwlsdl <heejin094@naver.com>
2024-07-10 10:52:34 +0900

    ssense done

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 import os
+import time
 
 # 올바른 ChromeDriver 경로 설정
 driver_path = 'D:/IDA/chromedriver-win64/chromedriver-win64/chromedriver.exe'  # chromedriver.exe의 정확한 경로로 변경
@@ -22,10 +24,14 @@ driver = webdriver.Chrome(service=service, options=options)
 
 # 사이트 리스트
 urls = [
-    'https://www.hollisterco.com/shop/wd/p/seamless-fabric-longline-crew-t-shirt-56759328?categoryId=120721&faceout=model&seq=12', # 여성 반팔
-    'https://www.hollisterco.com/shop/wd/p/easy-crew-sweater-56740835?categoryId=12627&faceout=model&seq=04', # 여성 긴팔
-    'https://www.hollisterco.com/shop/wd/p/relaxed-cooling-tee-57410320?categoryId=166245&faceout=model&seq=04', # 남성 반팔
-    'https://www.hollisterco.com/shop/wd/p/relaxed-atlanta-1996-olympics-graphic-crew-sweatshirt-57206820?categoryId=12572&faceout=model&seq=02', # 남성 긴팔
+    'https://www.ssense.com/en-kr/women/product/versace-jeans-couture/white-institutional-logo-t-shirt/15834731', # 여성 반팔
+    'https://www.ssense.com/en-kr/women/product/khaite/black-the-jo-sweater/15993171', # 여성 긴팔
+    'https://www.ssense.com/en-kr/women/product/simone-rocha/black-utility-bloomer-shorts/16589711', # 여성 반바지
+    'https://www.ssense.com/en-kr/women/product/khaite/blue-the-abigail-stretch-jeans/15992821', # 여성 긴바지
+    'https://www.ssense.com/en-kr/men/product/ashley-williams/sense-exclusive-gray-poodle-t-shirt/16891751', # 남성 반팔
+    'https://www.ssense.com/en-kr/men/product/sunspel/black-classic-long-sleeve-t-shirt/15836911', # 남성 긴팔
+    'https://www.ssense.com/en-kr/men/product/balenciaga/gray-camo-shorts/15668531', # 남성 반바지
+    'https://www.ssense.com/en-kr/men/product/jw-anderson/gray-twisted-workwear-jeans/16277131' # 남성 긴바지
 ]
 
 # 데이터 저장을 위한 리스트
@@ -33,11 +39,36 @@ all_data = []
 
 # 폴더 생성
 output_folder = 'output'
-os.makedirs(output_folder, exist_ok=True)
+
+def click_element_js(driver, element):
+    try:
+        driver.execute_script("arguments[0].click();", element)
+    except Exception as e:
+        print(f"An error occurred while trying to click the element: {e}")
+
+def extract_size_chart(driver):
+    size_data = []
+    size_elements = driver.find_elements(By.XPATH, "//div[@class='pdp-size-chart__guide-image-measurements']//ul//li")
+    for element in size_elements:
+        try:
+            style = element.get_attribute("style")
+            text = element.text.strip()
+            left = style.split("left:")[1].split("%")[0].strip()
+            top = style.split("top:")[1].split("%")[0].strip()
+            size_data.append({"left": left, "top": top, "text": text})
+        except:
+            continue
+    return size_data
 
 for url in urls:
     # 웹 페이지 열기
     driver.get(url)
+
+    clickable = driver.find_element(By.ID, "kIjAXDfDWuFqlzS")
+    ActionChains(driver)\
+        .click_and_hold(clickable)\
+        .perform()
+
     try:
         cookie = WebDriverWait(driver, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//button[contains(@id, 'onetrust-accept-btn-handler')]"))
@@ -48,34 +79,35 @@ for url in urls:
     
     try:
         button = WebDriverWait(driver, 30).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[text()='Size Guide']"))
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'pdp-size-chart__model-wearing')]"))
         )
         button.click()
 
         button1 = WebDriverWait(driver, 30).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@data-testid, 'toggle-block-measurement-cm')]"))
+            EC.element_to_be_clickable((By.XPATH, "//li[contains(@class, 'btn-cm')]"))
         )
         button1.click()
 
-        # 테이블이 나타날 때까지 대기 (XPath 사용)
-        table = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, "//table[contains(@class, 'size-tables__table')]"))
-        )
+        # 활성화된 li 요소를 순회하며 클릭
+        size_buttons = driver.find_elements(By.XPATH, "//ul[@class='pdp-size-chart__size-buttons-list']/li[not(contains(@class, 'disabled'))]")
+        
+        for button in size_buttons:
+            click_element_js(driver, button)
+            time.sleep(2)  # 테이블이 로드되도록 대기
 
-        # 테이블 크롤링
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        data = []
+            # 사이즈 차트 데이터 크롤링
+            size_chart = extract_size_chart(driver)
+            data = []
 
-        for row in rows:
-            cols = row.find_elements(By.TAG_NAME, "td")
-            cols = [col.text for col in cols]
-            data.append(cols)
-
-        df = pd.DataFrame(data)
-        all_data.append(df)
-
-        print(f"Data from {url}:")
-        print(df)
+            # 데이터프레임으로 변환
+            if data:  # 데이터가 존재하는 경우에만 변환
+                df = pd.DataFrame(data)
+                df['Size Chart'] = str(size_chart)  # 사이즈 차트 데이터를 데이터프레임에 추가
+                all_data.append(df)
+                print(f"Data from {url}:")
+                print(df)
+            else:
+                print(f"No data found in the table at {url}")
 
     except Exception as e:
         print(f"An error occurred while processing {url}: {e}")
@@ -84,7 +116,7 @@ for url in urls:
 combined_df = pd.concat(all_data, ignore_index=True)
 
 # 엑셀 파일로 저장
-excel_path = os.path.join(output_folder, 'size_tables_hollisterco.xlsx')
+excel_path = os.path.join(output_folder, 'size_tables_ssense.xlsx')
 combined_df.to_excel(excel_path, index=False)
 
 # 드라이버 종료
