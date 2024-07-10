48bba3f9c01d904870f52fecbf523b4f90d7752e
gmlwlsdl <heejin094@naver.com>
2024-07-10 22:43:18 +0900

    todandco done

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ driver = webdriver.Chrome(service=service, options=options)
 
 # 사이트 리스트
 urls = [
-    'https://www.outdoorresearch.com/collections/womens-tops/products/womens-echo-t-shirt-287658', # 여성 상의
-    'https://www.outdoorresearch.com/collections/womens-bottoms/products/womens-ferrosi-pants-regular-287668', # 여성 하의
-    'https://www.outdoorresearch.com/collections/mens-tops/products/mens-astroman-air-short-sleeve-shirt-300940', # 남성 상의
-    'https://www.outdoorresearch.com/collections/mens-bottoms/products/mens-ferrosi-pants-32-287641', # 남성 하의
+    'https://www.toadandco.com/products/manzana-ruffle-tank-egret?variant=41058789818416', # 여성 상의
+    'https://www.toadandco.com/products/earthworks-wide-leg-pant-pasture?variant=40701480075312', # 여성 하의
+    'https://www.toadandco.com/products/fletch-short-sleeve-shirt-north-shore-ditsy-print?variant=41058828779568', # 남성 상의
+    'https://www.toadandco.com/products/mission-ridge-pant-twine?variant=32689382260784', # 남성 하의
 ]
 
 # 데이터 저장을 위한 리스트
@@ -96,7 +96,7 @@ if all_data:
     combined_df = pd.concat(all_data, ignore_index=True)
 
     # 엑셀 파일로 저장
-    excel_path = os.path.join(output_folder, 'size_table_outdoorresearch.xlsx')
+    excel_path = os.path.join(output_folder, 'size_table_todandco.xlsx')
     combined_df.to_excel(excel_path, index=False)
 
     print(f"Data saved to {excel_path}")
